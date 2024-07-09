e5c16a89e232d296be15861ee9212b40cbebc72d
justputoff <kholid.hasibuan35@gmail.com>
2024-07-09 16:00:17 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::with('role')->get();
+        $users = User::with('role')->whereNot('role_id', 1)->get();
         return view('pages.users.index', compact('users'));
     }
 
