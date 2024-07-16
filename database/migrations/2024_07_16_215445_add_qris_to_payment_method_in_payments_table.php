a2131fc42540d581fcb47b0b136ed813b6d2d45c
justputoff <kholid.hasibuan35@gmail.com>
2024-07-17 05:33:49 +0700

    Update 2024_07_16_215445_add_qris_to_payment_method_in_payments_table.php

--- database/migrations/2024_07_16_215445_add_qris_to_payment_method_in_payments_table.php
+++ database/migrations/2024_07_16_215445_add_qris_to_payment_method_in_payments_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,18 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // Update existing rows to ensure they comply with the new constraint
-        DB::table('payments')->whereNotIn('payment_method', ['cash', 'bank_transfer', 'qris'])
-            ->update(['payment_method' => 'cash']); // or any default value
-
-        // Drop the existing constraint if it exists
-        DB::statement('ALTER TABLE payments DROP CONSTRAINT IF EXISTS payment_method_check');
-
-        // Alter the column to change its type to VARCHAR
-        DB::statement("ALTER TABLE payments ALTER COLUMN payment_method TYPE VARCHAR(255) USING payment_method::VARCHAR");
-
-        // Add the new constraint
-        DB::statement("ALTER TABLE payments ADD CONSTRAINT payment_method_check CHECK (payment_method IN ('cash', 'bank_transfer', 'qris'))");
+        Schema::table('payments', function (Blueprint $table) {
+            $table->enum('payment_method', ['cash', 'bank_transfer', 'qris'])->change();
+        });
     }
 
     /**
@@ -31,13 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        // Drop the existing constraint if it exists
-        DB::statement('ALTER TABLE payments DROP CONSTRAINT IF EXISTS payment_method_check');
-
-        // Revert the column to its original state
-        DB::statement("ALTER TABLE payments ALTER COLUMN payment_method TYPE VARCHAR(255) USING payment_method::VARCHAR");
-
-        // Optionally, you can add the original constraint back if needed
-        DB::statement("ALTER TABLE payments ADD CONSTRAINT payment_method_check CHECK (payment_method IN ('cash', 'bank_transfer'))");
+        Schema::table('payments', function (Blueprint $table) {
+            $table->enum('payment_method', ['cash', 'bank_transfer'])->change();
+        });
     }
 };
